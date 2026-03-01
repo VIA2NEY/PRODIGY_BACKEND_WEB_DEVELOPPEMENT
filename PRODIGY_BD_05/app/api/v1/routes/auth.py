f27a74c5bfb2a055afb18a02ec9154f3fba53b68
VIA2NEY <kouameangeV11@gmail.com>
2026-03-01 23:43:08 +0000

    preparing for v2

--- PRODIGY_BD_05/app/api/v1/routes/auth.py
+++ PRODIGY_BD_05/app/api/v1/routes/auth.py
@@ -1,18 +1,15 @@
 from fastapi import APIRouter, Depends, HTTPException
+from app.api.v1.dependencies import get_auth_service_v1
 from app.api.v1.schemas.auth_shema import LoginRequest, RegisterDetailResponse, TokenDetailResponse, UserRole
-from app.application.services.v1.auth_service import AuthService
-from app.infrastructure.repositories.user_repository import UserRepository
 from app.infrastructure.database.session import get_db
 from sqlalchemy.orm import Session
 from app.utils.response import ApiResponse
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
-def get_auth_service():
-    return AuthService(UserRepository())
 
 @router.post("/register", response_model=RegisterDetailResponse)
-def register(payload: LoginRequest, role: UserRole = UserRole.USER, service=Depends(get_auth_service), db: Session = Depends(get_db)):
+def register(payload: LoginRequest, role: UserRole = UserRole.USER, service=Depends(get_auth_service_v1), db: Session = Depends(get_db)):
     user = service.register(db, payload.email, payload.password, role)
     if not user:
         raise HTTPException(
@@ -28,7 +25,7 @@ def register(payload: LoginRequest, role: UserRole = UserRole.USER, service=Depe
 
 @router.post("/login", response_model=TokenDetailResponse)
 def login(payload: LoginRequest, db: Session = Depends(get_db)):
-    token = get_auth_service().login(db, payload)
+    token = get_auth_service_v1().login(db, payload)
     if not token:
         raise HTTPException(
             status_code=401,
